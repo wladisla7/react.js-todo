30dd51553fa927264eb3cddcab8944de0d31b1d4
f0rest <wladislawfedoseev@gmail.com>
2022-07-20 05:24:53 +0300

    add map for tasks

--- src/component/Tasks/index.jsx
+++ src/component/Tasks/index.jsx
@@ -1,25 +1,26 @@
-
 import React from 'react'
 import checkpic from '../../assets/img/check.png'
 import './tasks.scss'
 
-const Tasks = () => {
+const Tasks = ({ listItem }) => {
     return (
         <div className="todoTasks">
             <h2 className="task">
-                Фронтенд
+                {listItem.name}
             </h2>
 
             <div className="taskItem">
-                <div className="tasksItemRow">
-                    <div className="checkbox">
-                        <input id="check" type="checkbox" />
-                        <label htmlFor="check">
-                            <img src={checkpic} alt="checkpic" className="checkpic" />
-                        </label>
-                        <p>ReactJS Hooks</p>
+                {listItem.tasks.map(task =>
+                    <div key={task.id} className="tasksItemRow">
+                        <div className="checkbox">
+                            <input id={`task-${task.id}`} type="checkbox" />
+                            <label htmlFor={`task-${task.id}`}>
+                                <img src={checkpic} alt="checkpic" className="checkpic" />
+                            </label>
+                        </div>
+                        <span className="taskText">{task.text}</span>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     )
