598eaf414439610961ef3ef285b6a29136d4bb64
f0rest <wladislawfedoseev@gmail.com>
2022-07-20 05:24:27 +0300

    add axios delete for item

--- src/component/List/index.jsx
+++ src/component/List/index.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
+import axios from 'axios'
 import classNames from 'classnames';
+
 import Badge from '../Badge/index';
 import './list.scss';
 import deletePic from '../../assets/img/delete.png'
-import './list.scss';
+
 
 
 const List = ({ items, isDeleted, onClick, onRemove }) => {
-
     const deleteList = (item) => {
-        if (window.confirm('Вы действительно хотите удалить?')){
-            onRemove(item);
+        if (window.confirm('Вы действительно хотите удалить?')) {
+            axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+                onRemove(item.id);
+            });
         }
     }
 
     return (
         <ul onClick={onClick} className="todoList" >
-            {items.map((item, id) => (
-                <li key={id} className={classNames(item.className, { 'active': item.active })}>
+            {items.map((item, index) => (
+                <li key={index} className={classNames(item.className, { 'active': item.active })}>
                     <div className="listpic">
                         {item.icon ? (
                             item.icon
                         ) : (
-                            <Badge color={item.color} />
+                            <Badge color={item.color.name} />
                         )}
                     </div>
                     <span>{item.name}</span>
