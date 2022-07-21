5f90a83c3755639c43fb334387fe04fa82cdc153
f0rest <wladislawfedoseev@gmail.com>
2022-07-21 03:33:30 +0300

    add onEditTitle and onAddTask

--- src/component/Tasks/index.jsx
+++ src/component/Tasks/index.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import axios from 'axios'
+import AddTaskForm from '../AddTaskForm'
+
 import checkpic from '../../assets/img/check.png'
+import editpic from '../../assets/img/edit.png'
+
+
 import './tasks.scss'
 
-const Tasks = ({ listItem }) => {
+const Tasks = ({ listItem, onEditTitle, onAddTask }) => {
+
+    const editTitle = () => {
+        const newTitle = window.prompt('Название списка', listItem.name);
+        if (newTitle) {
+            onEditTitle(listItem.id, newTitle);
+            axios.patch('http://localhost:3001/lists/' + listItem.id, {
+                name: newTitle
+            }).catch(() => {
+                alert('Ошибка запроса')
+            })
+        }
+    }
+
     return (
         <div className="todoTasks">
             <h2 className="task">
                 {listItem.name}
+                <img
+                    onClick={editTitle}
+                    src={editpic}
+                    alt="edit pic" />
             </h2>
 
             <div className="taskItem">
+                {!listItem.tasks.length && <h3>Задачи отсутствуют</h3>}
                 {listItem.tasks.map(task =>
                     <div key={task.id} className="tasksItemRow">
                         <div className="checkbox">
@@ -21,6 +45,7 @@ const Tasks = ({ listItem }) => {
                         <span className="taskText">{task.text}</span>
                     </div>
                 )}
+                <AddTaskForm listItem={listItem} onAddTask={onAddTask} />
             </div>
         </div>
     )
